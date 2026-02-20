3566c397b009dece4252db050ee0e498baa5d784
Jasper <jasperhartong+private@gmail.com>
2026-02-20 14:44:48 +0100

    Add baseline dimensions file for HSB wand and clean up command.rb. Introduced a new text file containing total linear dimensions and their respective details. Removed unnecessary whitespace in command.rb for improved readability.

--- sketchup_bridge/command.rb
+++ sketchup_bridge/command.rb
@@ -1,6 +1,5 @@
 core = File.expand_path('../plugins/timmerman_skeleton_dimensions/core.rb', __dir__)
 load core
-
 Timmerman::SkeletonDimensions.debug_mode = true
 Timmerman::SkeletonDimensions.clear
 Timmerman::SkeletonDimensions.run
